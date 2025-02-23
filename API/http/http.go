b424509a0eba5f7f0aaab17e90cc7528a54c7c8a
Tousart <BubnovSeva@yandex.ru>
2025-02-23 21:37:29 +0300

    [Project] add comment

--- API/http/http.go
+++ API/http/http.go
@@ -70,6 +70,7 @@ func (s *Tasks) getHandlerResult(w http.ResponseWriter, r *http.Request) {
 // @Failure 500 {string} string "Internal Server Error"
 // @Router /task [post]
 func (s *Tasks) postHandler(w http.ResponseWriter, r *http.Request) {
+	// fake comment
 	task := domain.CreateTask()
 	err := s.service.Post(task)
 	types.ProcessError(w, err, &types.GetTaskIdHandler{Value: task.Task_id})
